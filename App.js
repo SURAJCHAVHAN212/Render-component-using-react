4919f6803938a8a0aaee8a5adafad35176ea2131
Suraj <97757309+SURAJCHAVHAN212@users.noreply.github.com>
2022-06-29 14:23:03 +0530

    Add files via upload
    Render- component

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+ReactDom.render(<p>I am learning React. My life is getting better.
+</p>,
+document.getElementById('root'));
+
+export default App;
